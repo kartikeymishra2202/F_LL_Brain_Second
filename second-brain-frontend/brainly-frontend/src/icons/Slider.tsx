6e5cf39458e6383878fee735a6ba94ab7c9234a8
kartikeymishra2202 <mharmilap@gmail.com>
2025-01-27 20:50:48 +0530

    added some deployment fix

--- second-brain-frontend/brainly-frontend/src/icons/Slider.tsx
+++ second-brain-frontend/brainly-frontend/src/icons/Slider.tsx
@@ -1,4 +1,4 @@
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-export function Slider() {
-  return <FontAwesomeIcon icon="fa-solid fa-sliders-simple" />;
-}
+// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// export function Slider() {
+//   return <FontAwesomeIcon icon="fa-solid fa-sliders-simple" />;
+// }
