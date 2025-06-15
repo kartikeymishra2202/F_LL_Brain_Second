179a2f2eefe700adfb54fecc35507cfbc2d364f4
kartikeymishra2202 <mharmilap@gmail.com>
2025-06-15 21:53:37 +0530

    fixing svg errors

--- second-brain-frontend/brainly-frontend/src/icons/crossIcon.tsx
+++ second-brain-frontend/brainly-frontend/src/icons/crossIcon.tsx
@@ -9,8 +9,8 @@ export function CrossIcon() {
       className="size-6"
     >
       <path
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeLinecap="round"
+        strokeLinejoin="round"
         d="M6 18 18 6M6 6l12 12"
       />
     </svg>
